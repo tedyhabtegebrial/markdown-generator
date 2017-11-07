8de257575baf8255ced75582d58c28406c95d474
cmccandless <corey.mccandless@harman.com>
2017-11-07 16:52:27 -0600

    bump build number

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('LICENSE') as f:
 
 setup(
     name='markdown_generator',
-    version='0.1.0',
+    version='0.1.1',
     description='Python package for generating GitHub-flavored markdown',
     long_description=readme,
     author='Corey McCandless',
